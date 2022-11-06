66750d8875eaf27779cd8e2b7330987729ecef18
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2022-11-06 21:10:34 +0100

    Solved lab

--- js/index.js
+++ js/index.js
@@ -1,42 +1,62 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(".price span").innerText
+  const quantity = product.querySelector(".quantity input").value
+  const subTotal = product.querySelector(".subtotal span")
+  const subTotalCount = price * quantity
 
-  //... your code goes here
+  subTotal.innerText = subTotalCount
+
+  return subTotalCount
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.querySelectorAll('.product');
+  const total = document.querySelector('#total-value span')
+  let totalCount = 0
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  products.forEach(product => totalCount += updateSubtotal(product))
+  total.innerText = totalCount
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const removeProduct = event.path[2]
+  removeProduct.remove()
 }
 
+function clickRemoveProduct() {
+  const actionListener = document.querySelectorAll(".action")
+
+  actionListener.forEach(action => {
+    action.addEventListener('click', (event) => {
+      removeProduct(event)
+    });
+  })
+}
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const createProduct = document.querySelector("#cart tbody")
+  const productName = document.querySelectorAll(".create-product input")[0]
+  const unitPrice = document.querySelectorAll(".create-product input")[1]
+
+  createProduct.insertAdjacentHTML('beforeend', `<tr class="product"><td class= "name"><span>${productName.value}</span></td><td class="price">$<span>${unitPrice.value}</span></td><td class="quantity"><input type="number" value="0" min="0" placeholder="Quantity" /></td><td class="subtotal">$<span>0</span></td><td class="action"><button class="btn btn-remove">Remove</button></td></tr>`)
+
+  productName.value = ""
+  unitPrice.value = ""
 }
+const createButton = document.querySelector("#create")
+
+createButton.addEventListener('click', () => {
+  createProduct()
+  clickRemoveProduct()
+});
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  clickRemoveProduct()
 });
